e74bfa5ab315d7d8502e2925cee36ff8dc32e958
MD Omar Faruk <muhammadabuhafz@gmail.com>
2026-09-07 09:19:21 +0600

    fixed issues

--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,6 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.post("/", async (req, res) => {
-    const body = req.body;
-    const user = await createUser(body);
-    
-    res.status(201).json({
-      success: true,
-      message: "User created successfully",
-      data: user
-  })
-});
-
-const name:string=10
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
